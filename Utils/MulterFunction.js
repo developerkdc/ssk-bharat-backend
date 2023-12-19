54b2d3cf680e8720fd7e6bb8adae8a4a562e4bb4
AbhiKdc <abhishek@kdigitalcurry.com>
2023-12-19 10:11:02 +0530

    multer

--- Utils/MulterFunction.js
+++ Utils/MulterFunction.js
@@ -2,19 +2,19 @@ import fs from "fs";
 import multer from "multer";
 
 export const MulterFunction = (dist) => {
-    if (fs.existsSync(dist)) {
-        const storage = multer.diskStorage({
-            destination: function (req, file, cb) {
-                cb(null, dist)
-            },
-            filename: function (req, file, cb) {
-                cb(null, `${Date.now()}-${file.originalname}`)
-            }
-        });
-        const upload = multer({ storage: storage });
-        return upload
-    } else {
+
+    if(!fs.existsSync(dist)){
         fs.mkdirSync(dist, { recursive: true });
-        return MulterFunction(dist)
     }
+
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, dist)
+        },
+        filename: function (req, file, cb) {
+            cb(null, `${Date.now()}-${file.originalname}`)
+        }
+    });
+    const upload = multer({ storage: storage });
+    return upload
 }
