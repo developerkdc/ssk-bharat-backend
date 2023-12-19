af76e8837f7d99ac4cfa961439bfa0afaabb8a7f
PrathameshTharali <prathamesh@kdigitalcurry.com>
2023-12-19 12:38:00 +0530

    roles api done

--- database/schema/roles.schema.js
+++ database/schema/roles.schema.js
@@ -1,39 +1,38 @@
 import mongoose from "mongoose";
 
-var RoleSchema = new mongoose.Schema({
-  role_name: { type: String, min: 2, max: 25, required: true },
+const RoleSchema = new mongoose.Schema({
+  role_name: { type: String, min: 2, max: 25, required: [true,"Role Name is required"],unique:true},
   permissions: {
-    type: {
-        user:{
-          add:{type:Boolean,default:false},
-          edit:{type:Boolean,default:false},
-          view:{type:Boolean,default:false}
-        },
-        roles:{
-          add:{type:Boolean,default:false},
-          edit:{type:Boolean,default:false},
-          view:{type:Boolean,default:false}
-        },
-        suppliers:{
-          add:{type:Boolean,default:false},
-          edit:{type:Boolean,default:false},
-          view:{type:Boolean,default:false}
-        },
-        sskcompany:{
-          add:{type:Boolean,default:false},
-          edit:{type:Boolean,default:false},
-          view:{type:Boolean,default:false}
-        },
-        retailer:{
-          add:{type:Boolean,default:false},
-          edit:{type:Boolean,default:false},
-          view:{type:Boolean,default:false}
-        },
+    user: {
+      add: { type: Boolean, default: false },
+      edit: { type: Boolean, default: false },
+      view: { type: Boolean, default: false },
+    },
+    roles: {
+      add: { type: Boolean, default: false }, 
+      edit: { type: Boolean, default: false },
+      view: { type: Boolean, default: false },
+    },
+    suppliers: {
+      add: { type: Boolean, default: false },
+      edit: { type: Boolean, default: false },
+      view: { type: Boolean, default: false },
+    },
+    sskcompany: {
+      add: { type: Boolean, default: false },
+      edit: { type: Boolean, default: false },
+      view: { type: Boolean, default: false },
+    },
+    retailer: {
+      add: { type: Boolean, default: false },
+      edit: { type: Boolean, default: false },
+      view: { type: Boolean, default: false },
     },
   },
-  created_at: { type: Date, default: Date.now() },
-  updated_at: { type: Date, default: Date.now() },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
   deleted_at: { type: Date, default: null },
 });
 
-export default mongoose.model("RoleModel", RoleSchema, "roles");
+const rolesModel = mongoose.model("roles", RoleSchema);
+export default rolesModel;
--- routes/Admin/RolesRoutes.js
+++ routes/Admin/RolesRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { createRole, getRoles,getRolesList } from "../../controllers/Admin/rolesController";
 const router = express.Router();
 
+router.post("/createRole",createRole)
+router.get("/getRoles",getRoles).get("/rolesList",getRolesList)
 
 export default router;
