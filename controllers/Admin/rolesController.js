09315a2a22022f008ccf1d35a47cec3a5d661914
PrathameshTharali <prathamesh@kdigitalcurry.com>
2023-12-19 14:23:41 +0530

    roles api search pagination done

--- controllers/Admin/rolesController.js
+++ controllers/Admin/rolesController.js
@@ -1,3 +1,4 @@
+import ApiError from "../../Utils/ApiError";
 import catchAsync from "../../Utils/catchAsync";
 import rolesModel from "../../database/schema/roles.schema";
 
@@ -16,12 +17,32 @@ export const createRole = catchAsync(async (req, res, next) => {
 
 export const getRoles = catchAsync(async (req, res, next) => {
   console.log("iuhrgiuh");
-  const role = await rolesModel.find();
+
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const sortDirection = req.query.sort === 'desc' ? -1 : 1;
+  const search = req.query.search || '';
+  const searchQuery = search ? { role_name: { $regex: search, $options: 'i' } } : {};
+  const totalRoles = await rolesModel.countDocuments(searchQuery);
+  const totalPages = Math.ceil(totalRoles / limit);
+  const validPage = Math.min(Math.max(page, 1), totalPages);
+  const skip = (validPage - 1) * limit;
+
+  const role = await rolesModel
+    .find(searchQuery)
+    .sort({ role_name: sortDirection })
+    .skip(skip)
+    .limit(limit);
+
   if (role) {
-    return res.status(201).json({
-      statusCode: 201,
+    return res.status(200).json({
+      statusCode: 200,
       status: true,
-      data: role,
+      data: {
+        roles:role,
+        totalPages:totalPages,
+        currentPage: validPage,
+      },
       message: "All Roles and Permissions",
     });
   }
@@ -35,15 +56,32 @@ export const getRolesList = catchAsync(async (req, res, next) => {
         _id: 1,
         role_name: 1,
       },
-    }
+    },
   ]);
-  
+
   if (role) {
-    return res.status(201).json({
-      statusCode: 201,
+    return res.status(200).json({
+      statusCode: 200,
       status: true,
       data: role,
-      message: "All Roles and Permissions",
+      message: "All Roles List",
     });
   }
+});
+
+export const updateRole = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const role = await rolesModel.findByIdAndUpdate(
+    id,
+    { ...req.body, updated_at: Date.now() },
+    { new: true }
+  );
+  console.log(role);
+  if (!role) return next(new ApiError("Role Not Found", 404));
+  return res.status(200).json({
+    statusCode: 200,
+    status: true,
+    data: role,
+    message: "Role Updated",
+  });
 });
