9e14fefac492f74d0b887f7c354addf11785bdb4
AbhiKdc <abhishek@kdigitalcurry.com>
2024-01-06 18:12:38 +0530

    auth

--- controllers/Retailers/Auth/Auth.controller.js
+++ controllers/Retailers/Auth/Auth.controller.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 import ApiError from "../../../Utils/ApiError";
 import catchAsync from "../../../Utils/catchAsync";
-import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
-import { auth } from "../../../Firebase/FirebaseConfig";
 
 export const RetailerLogin = catchAsync(async (req,res,next)=>{
     const {mobileNo} = req.body;
@@ -10,24 +8,7 @@ export const RetailerLogin = catchAsync(async (req,res,next)=>{
     const retailer = await mongoose.model("retailers").findOne({"current_data.register_mobile_no":mobileNo},{"current_data.register_mobile_no":1});
     if(!retailer) return next(ApiError("This mobile number is not exists",400));
 
-    const appVerifier = new RecaptchaVerifier('recaptcha-container', {
-        size: 'invisible',
-        callback: (response) => {
-          console.log(response)
-        },
-        'expired-callback': () => {
-          // reCAPTCHA verification expired, handle it if needed
-        }
-      });
-
-    signInWithPhoneNumber(auth,mobileNo,appVerifier).then((confirmationResult) => {
-        return res.status(200).json({
-            message:"OTP send successfully",
-            confirmationResult
-        })
-      }).catch((error) => {
-        return next(error)
-      });
+    res.send("login")
 
     
 })
